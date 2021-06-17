e69da97f571fdc81ae02c972b5b110ebaec2cf97
jessijoke <jessijoke@yahoo.com>
2021-06-17 03:19:48 -0400

    added route for looking up movie reservations by reservation number

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :reservations
 
   get 'reservations/search/:reservation_number/', to: 'reservations#look_up_reservation'
+  get 'movieres/search/:reservation_number/', to: 'movieres#look_up_reservation'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
